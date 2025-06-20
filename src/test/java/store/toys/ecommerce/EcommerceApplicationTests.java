fc2febf307091a4b0382e9905ab0251153bb0a9a
vitaFlash <vitaliaflash@gmail.com>
2025-06-20 14:29:26 +0200

    first commit

---
+++ src/test/java/store/toys/ecommerce/EcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package store.toys.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
