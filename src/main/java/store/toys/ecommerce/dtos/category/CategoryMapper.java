d50b3e0b65abe08ea684b0ac823c4740566bc50e
Nia <niaespinal@outlook.com>
2025-06-27 12:25:33 +0200

    TOYS-27 Add return null to ControllerMapper

--- src/main/java/store/toys/ecommerce/dtos/category/CategoryMapper.java
+++ src/main/java/store/toys/ecommerce/dtos/category/CategoryMapper.java
@@ -5,6 +5,9 @@ import store.toys.ecommerce.models.Category;
 public class CategoryMapper {
 
     public static Category toEntity(CategoryDTO dto) {
+        if (dto == null) {
+            return null;
+        }
         return Category.builder()
                 .name(dto.getName())
                 .build();
